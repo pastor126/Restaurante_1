bd5fc2b00e44084f375f332a35369fd0b1db9ba2
Eduardo C Pacheco <pastor126@gmail.com>
2023-01-24 22:40:21 -0400

    Add coment dados de conexão do DB.

--- conectaDB.php
+++ conectaDB.php
@@ -7,4 +7,12 @@ try {
     echo '<br>Falha na conexão';
     die($e->getMessage());
 }
+
+
+// postgresql://
+// postgres:
+// g1d9I4yfWFjgxw4WY805@
+// containers-us-west-72.railway.app:6239/railway/
+
+
 ?>
